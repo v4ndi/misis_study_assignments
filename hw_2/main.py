0f14ecb5b388710f836fb47249311161330735b1
Nikita Rusakov <rusakov0609@gmail.com>
2023-09-30 23:31:33 +0300

    convert ipynb to .py and add logging

---
+++ hw_2/main.py
@@ -0,0 +1,116 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+import csv
+from datetime import datetime
+import logging
+from utils import get_weather, get_user_city, read_config
+
+logging.basicConfig(filename='bot_logs.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8-sig')
+
+def main():
+    config = read_config()
+    group_key = config.get('Credentials', 'group_key')
+
+    vk_session = vk_api.VkApi(token=group_key)
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+
+    keyboard = VkKeyboard(one_time=False)
+    keyboard.add_button('Погода в моем городе', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_button('Погода в другом городе', color=VkKeyboardColor.POSITIVE)
+
+    waiting_for_user_input = {}
+    users_city_requests = {}
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            if event.user_id not in waiting_for_user_input.keys():
+                waiting_for_user_input[event.user_id] = False
+                users_city_requests[event.user_id] = ''
+                
+                vk.messages.send(
+                    keyboard=keyboard.get_keyboard(),
+                    key=("eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJxdWV1ZV9pZCI6IjIyMjU2MTU1MSIsInVudGlsIjoxNjk0NzcyMTIxMTkxMTE0NjY1fQ.K3B7_Qr4c6_T0u0qILeOyKi1mzY-kOA712aQixVmGoK3k4_tNLXnWIkgW9HF95DEFrJJpfMSogljPa9CM1F2DQ"),
+                    server=("https://lp.vk.com/whp/222561551"),
+                    ts=("42"),
+                    user_id=event.user_id,
+                    random_id=get_random_id(),
+                    message='Добро пожаловать в weather-BOT!'
+                )
+                log_data = {
+                    'user_id': event.user_id,
+                    'info': 'User joined the bot',
+                    'step': 'Welcome'
+                }
+                logging.info(log_data)
+
+            if event.text.startswith('Погода в моем городе'):
+                log_data = {
+                    'user_id': event.user_id,
+                    'info': 'User requested weather in their city. Waiting interval',
+                    'step': 'Weather in the city from profile'
+                }
+                logging.info(log_data)
+
+                waiting_for_user_input[event.user_id] = True
+
+                vk.messages.send(
+                    user_id=event.user_id,
+                    random_id=get_random_id(),
+                    message='Введите интервал прогноза погоды в днях от 1 до 10'
+                )
+
+            elif waiting_for_user_input[event.user_id]:
+                try:
+                    interval = int(event.text)
+                except ValueError:
+                    interval = -1
+
+                if users_city_requests[event.user_id] == '':
+                    user_city = get_user_city(event.user_id, group_key=group_key)
+                else:
+                    user_city = users_city_requests[event.user_id]
+
+                vk.messages.send(
+                    user_id=event.user_id,
+                    random_id=get_random_id(),
+                    message=get_weather(city=user_city, interval=interval, user_id=event.user_id)
+                )
+
+                waiting_for_user_input[event.user_id] = False
+                users_city_requests[event.user_id] = ''
+
+            elif event.text.startswith('Погода в другом городе'):
+                log_data = {
+                    'user_id': event.user_id,
+                    'info': 'User requested weather in another city. Waiting name of the city',
+                    'step': 'Weather in another city'
+                }
+                logging.info(log_data)
+
+                vk.messages.send(
+                    user_id=event.user_id,
+                    random_id=get_random_id(),
+                    message='Введите название города в формате "город: Москва"'
+                )
+
+            elif event.text.startswith('город:'):
+                if len(event.text) >= 7:
+                    log_data = {
+                        'user_id': event.user_id,
+                        'info': 'Got city from user input',
+                        'step': 'Weather in another city'
+                    }
+                    city = event.text[6:].strip()
+                    users_city_requests[event.user_id] = city
+                    waiting_for_user_input[event.user_id] = True
+                    vk.messages.send(
+                        user_id=event.user_id,
+                        random_id=get_random_id(),
+                        message='Введите интервал прогноза погоды в днях от 1 до 10'
+                    )
+
+if __name__ == '__main__':
+    main()
---
+++ hw_2/utils.py
@@ -0,0 +1,92 @@
+import requests
+from bs4 import BeautifulSoup
+import configparser
+import logging
+import os
+
+def get_weather(city: str, interval: int, user_id: int) -> str:
+    if interval < 1 or interval > 10:
+        log_data = {
+            'user_id': user_id,
+            'info': f'User pass invalid interval = {interval}',
+            'step': 'Getting interval'
+        }
+        logging.info(log_data)
+        return 'Введен неверный интервал'
+    else:
+        log_data = {
+            'user_id': user_id,
+            'info': f'User pass correct interval = {interval}',
+            'step': 'Getting interval'
+        }
+        logging.info(log_data)
+    
+    city = city.lower()
+    request = requests.get(f'https://sinoptik.ua/погода-{city}/10-дней')
+
+    if request.status_code != 404:
+        log_data = {
+            'user_id': user_id,
+            'info': 'response to weather-web-service was success',
+            'step': 'Send weather to user'
+        }
+        logging.info(log_data)
+
+    b = BeautifulSoup(request.text, "html.parser")
+    
+    if b.find('div', class_='r404') != None:
+        result = 'Город не найден попробуйте снова'
+        log_data = {
+            'user_id': user_id,
+            'info': 'The city was not found on the site',
+            'step': 'Send the weather'
+        }
+        logging.info(log_data)
+        return result
+        
+    p3 = b.findAll('div', class_='main')
+    result = f'Погода в городе {city.capitalize()} \n'
+    
+    for div in p3[0:interval]:
+        day = div.find('p', class_='date').get_text()
+        month = div.find('p', class_='month').get_text()
+        
+        result += f'{day}, {month}: \n'
+        morning_temp, day_temp = (x.get_text() for x in div.find_all('span'))
+        result += f'Мин: {morning_temp} Макс: {day_temp} \n'
+
+    return result
+
+def get_user_city(user_id, group_key):
+    user_info = requests.get('https://api.vk.com/method/users.get', params={
+        'user_ids': user_id,
+        'fields': 'city',
+        'v':5.154,
+        'access_token': group_key
+    })
+
+    city = user_info.json()['response'][0]['city']['title']
+
+    if city == None:
+        log_data = {
+            'user_id': user_id,
+            'info': 'City from profile doesn\'t find',
+            'step': 'Getting city from user profile'
+        }
+        logging.info(log_data)
+    else:
+        log_data = {
+            'user_id': user_id,
+            'info': 'The city was found',
+            'step': 'Getting city from user profile'
+        }
+        logging.info(log_data)
+
+    return city 
+
+def read_config(filename='config.ini'):
+    config = configparser.ConfigParser()
+
+    if os.path.exists(filename):
+        config.read(filename)
+        return config
